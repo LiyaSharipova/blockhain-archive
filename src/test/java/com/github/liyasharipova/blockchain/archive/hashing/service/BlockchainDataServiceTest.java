79a13c217114481202edff0eae259f75cb2f3cbb
liyasharipova <liyasharipova2107@gmail.com>
2018-03-04 15:13:04 +0300

    BlockchainDataServiceTest добавил

--- src/test/java/com/github/liyasharipova/blockchain/archive/hashing/service/BlockchainDataServiceTest.java
+++ src/test/java/com/github/liyasharipova/blockchain/archive/hashing/service/BlockchainDataServiceTest.java
@@ -1,13 +1,27 @@
 package com.github.liyasharipova.blockchain.archive.hashing.service;
 
+import com.github.liyasharipova.blockchain.archive.AppConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Date;
+
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppConfig.class})
 public class BlockchainDataServiceTest {
+
+    @Autowired
+    private BlockchainDataService blockchainDataService;
 
     @Test
     public void placeToBlockchain() {
+
+        byte[] fileData = "test-data".getBytes();
+        String userId = "test-user-id";
+        long currentTimeInSec = new Date().getTime();
+        blockchainDataService.placeToBlockchain(fileData, userId, currentTimeInSec);
     }
 }
