593af09e520c0627684626cf36e63c3f03463a27
liyasharipova <liyasharipova2107@gmail.com>
2018-03-04 14:47:27 +0300

    init.sql changed

--- src/main/resources/migrations/init.sql
+++ src/main/resources/migrations/init.sql
@@ -1,12 +1,30 @@
 -- CREATE DATABASE blockhain_archive;
 
-CREATE TABLE IF NOT EXISTS file
+
+CREATE SCHEMA blockchain_archive;
+CREATE TABLE IF NOT EXISTS blockchain_archive.transaction
 (
-  id BIGSERIAL PRIMARY KEY NOT NULL,
-  file_name TEXT NOT NULL,
-  data BYTEA NOT NULL
+  id         BIGSERIAL PRIMARY KEY NOT NULL,
+  file_hash  VARCHAR               NOT NULL,
+  block_hash VARCHAR               NOT NULL,
+  user_id    VARCHAR               NOT NULL,
+  tr_time    TIMESTAMP             NOT NULL,
+  file_name  TEXT                  NOT NULL,
+  file_data  BYTEA                 NOT NULL
 );
-CREATE INDEX IF NOT EXISTS file__file_name__ix ON file (file_name);
-COMMENT ON COLUMN file.file_name IS 'Название файла';
-COMMENT ON COLUMN file.data IS 'Данные файла в байтовом представлении';
-COMMENT ON TABLE file IS 'Загруженные файлы';
+
+CREATE INDEX IF NOT EXISTS transaction__file_name__ix
+  ON blockchain_archive.transaction (file_name);
+COMMENT ON COLUMN blockchain_archive.transaction.file_name IS 'Название файла';
+COMMENT ON COLUMN blockchain_archive.transaction.file_data IS 'Данные файла в байтовом представлении';
+COMMENT ON COLUMN blockchain_archive.transaction.file_hash IS 'Хэш файла';
+COMMENT ON TABLE blockchain_archive.transaction IS 'Транзакции загруженных файлов';
+
+
+CREATE TABLE blockchain_archive.block
+(
+  hash          VARCHAR PRIMARY KEY,
+  previous_hash VARCHAR NOT NULL
+);
+COMMENT ON COLUMN blockchain_archive.block.hash IS 'хеш блока';
+COMMENT ON COLUMN blockchain_archive.block.previous_hash IS 'хеш предыдущего блока';
